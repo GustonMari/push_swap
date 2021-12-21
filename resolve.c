d646fc863fb0c854ff753834e6d4478abf9dee6f
Gustave MARY <gmary@e2r6p20.clusters.42paris.fr>
2021-12-21 15:00:03 +0100

    21/12 aprem 2

--- resolve.c
+++ resolve.c
@@ -6,69 +6,41 @@
 /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 16:01:01 by gmary             #+#    #+#             */
-/*   Updated: 2021/12/21 13:39:22 by gmary            ###   ########.fr       */
+/*   Updated: 2021/12/21 14:59:32 by gmary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 // faire le double chainage pour scanner par la fin
+#include <stdio.h>
 
 void	ft_resolve_a(t_list	**head_a, t_list **head_b)
 {
-	int	mid;
-	t_list *temp;
+	int		mid;
+	t_list	*temp;
+	int		middle_size;
 	// surement passer par une valeur temp pour leak avec med_position
 	temp = *head_a;
 	mid = ft_med_position(&temp);
-	free(temp);	
-	//while (ft_already_sort(head_a))
-	//{
-	while ((*head_a)->content < mid)
-		ft_pb(head_a, head_b);
-	//temp = *head_a;
-	while ((*head_a)->content >= mid)
-	{	
-		if ((*head_a)->content < mid)
-			break;
-		if ((*head_a)->content >= mid)
-		{	
-			printf("mid =%d\n", mid);
+	middle_size = ft_lstsize(temp);
+	free(temp);
+	//if (ft_lstsize(*head_a) == 2)
+	//	return ;
+	while (ft)
+	{}
+		while ((*head_a)->content < mid)
+			ft_pb(head_a, head_b);
+		while (ft_lstlast(*head_a)->content < mid)
+		{
 			ft_rra(head_a);
+			ft_pb(head_a, head_b);
 		}
-		else
-			break;
-	}
-	//temp = *head_a;
-		
-		
-		
-	//while ((*head_a))
-	//{
-	//	printf("%d\n", ((*head_a)->content));
-	//	(*head_a) = (*head_a)->next;
-	//}
-	//printf("liste b\n");
-	//while ((*head_b))
-	//{
-	//	printf("%d\n", (*head_b)->content);
-	//	(*head_b) = (*head_b)->next;
-	//}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//while (temp->content < mid)
-		//{
-		//	ft_rra(head_a);
-		//	ft_pb(head_a, head_b);
-		//}
-
-
-	//}
+		while ((*head_a)->content >= mid)
+			ft_ra(head_a);
+		while ((*head_a)->content < mid)
+			ft_pb(head_a, head_b);
+	}	
+	//ft_resolve_a(head_a, head_b);
 }
+
