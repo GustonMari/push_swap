333e60b5224768d78327afd34650f98d44515200
Gustave MARY <gmary@e2r5p22.clusters.42paris.fr>
2022-01-13 09:27:22 +0100

    no leaks, working

--- ft_print_result.c
+++ ft_print_result.c
@@ -6,7 +6,7 @@
 /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 09:46:55 by gmary             #+#    #+#             */
-/*   Updated: 2022/01/07 11:25:09 by gmary            ###   ########.fr       */
+/*   Updated: 2022/01/13 09:20:05 by gmary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,29 @@ void	ft_putstr(char *str)
 
 void	ft_print_result(t_operation **head_op)
 {
+	t_operation	*temp;
+	
 	while (*head_op)
 	{
+		temp = *head_op;
+		ft_putstr((temp)->operation);
+		write(1, "\n", 1);
+		*head_op = (*head_op)->next;
+		free(temp);
+	}
+}
+
+/*
+void	ft_print_result(t_operation **head_op)
+{
+	t_operation	*temp;
+	
+	while (*head_op)
+	{
+		temp = *head_op;
 		ft_putstr((*head_op)->operation);
 		write(1, "\n", 1);
 		*head_op = (*head_op)->next;
 	}
 }
+*/
